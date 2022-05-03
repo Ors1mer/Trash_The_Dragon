3fc5d74bea6ba60df2f54a0eba77326c291b2611
Ors1mer <jakonhovland96@gmail.com>
2022-05-03 15:28:20 +0300

    Movement & directions added

--- painter.pas
+++ painter.pas
@@ -1,8 +1,9 @@
-unit dragon;
+unit painter;
 {$mode OBJFPC} { Used to create optional function arguments }
 interface
 
-procedure paint_dragon(x, y: integer; hide: boolean = false);
+procedure paint(x, y: integer; data: string);
+procedure paint_dragon(x, y, direction: integer; hide: boolean = false);
 procedure paint_image();
 
 implementation
@@ -62,39 +63,18 @@ begin
     TextBackground(Black); TextColor(LightGray);
 end;
 
-{
-°*o**.oo*oO°*oo.oO*.O* *O°.°°o°°o. .**o  oO°*  o° oO°oo*oOOO*° *@# oO**OOOOOO*°**oooO*°@@@@@@@@@@@@@@@@
-oo.°oo*o°oo°Oo**o°*O° °o°°*. oo.°o°.°*°oo °O.*  *° .o*°o*oOOOo o@@°.oOo*o***.°*OO**o*O**@@@@@@@@@@@@@@@
-*.*Oo°**°*o°O°.o°*o° *O°.o°  *o°..°* o.oo° O°** .*°°  .***OOOOo o@# .o#o.°*ooooOOo°O*oO.o@@@@@@@@@@@@@@
- *O**o*°o*oo*°*o**°.oO..°*O .o*.* .  ***Oo °o.*°°°o°o  *oO*oOOOO o@#*°*o.O*oOoOO° °oO*oO°@@@@@@@@@@@@@@
- Oo**o°o*°***o°oo° oo  °. *.*o°o.°.o.°°*oo. o.*°# *oO .  o@.OO**o***o#o.  oOO*** ***oo**O*@@@@@@@@@@@@@
-.oo°o*°*°o*°o°*o° °Oo  o   .O°*°  °. Oo°oo. o°O . .ooo*. #@.°ooo*oOo *@@@.°OO  o°o°**oO*o*o@@@@@@@@@@@@
-o.*°°.o*°o*°**o*.oO*  .o.**°° ..*.o °O°*Oo. .**  *°°°O°o °#@Ooo*Oo**O#@@O oo*.*°.*OoO**O*#*o@@@@@@@@@@@
-@#OO*°°o.*°°* *.°**o °°.* .o**. . .°o*°*OO  *°*° oO°*o*°O .o o@* O°°@@#..oOOO.°#o.°OoOo***#°O@@@@@@@@@@
-.°oooO°..°o*Oo**°* *° °*.°°..*Oo°.. .. °   .°** °ooooOOO*° °°O@.    o@@°.ooOOO*.O#*.o*Oo*oOO°#@@@@@@@@@
-oooOo°O#####OO#O °  **°o. .oooo*Oo..#°*O o°°°.**o.°..  °#* .O@o  °. .@@o.#**Oo°°O@@ °**oo*OOO.#@@@@@@@@
-*oo° O#Ooo*.°**OOO* *O. .*° °*o**°°.o.*O.ooo* . °.*o*°o  #@oo@° .°O .O@#.oOOo.#@#°°°ooo*oo*OOO°@@@@@@@@
- °°*.°°*oo° ..° °° °°.°ooo**o°****° °.°° *°°*o°  .*°@°°*. o##@@   o#o  #O  °*o.O@.*Ooo*ooOO*oOO°#@@@@@@
-****OOo*OO#O*o. O. °*ooO*°*o°*°°°*oo*°*o°.°*o**.  .°O#°**.°. .o@o   °O*#@@OoO°  #@..oOoo*OOOo*O#**@@@@@
-.OO°°*. **° °o°..   o*°*°*..° °*°°oo°Oo°*°o**.*o. ..*oOo° oo.° .#@O°  .*o@@@@o  o@#Oo.ooOoooOO*oO°°@@@@
-.oo.°*oooo*oo°°*o**o*o.*#° °°o*°°°***°*°**oo*o°***o... *@° o.**   °O@o    °o@@#@@##o°.oooOOoooo**Oo*@@@
-°°*.*o° .°*o°  °*#Oo°***O. OO°oo* *oo***o**OoOOoo°°o°°  @o.o°o*.°°   @@#o. *@#@O.. *    *ooOOooo***ooo#
- .°o° ° .*..°.*°. ..   ***o.. °oo°. oOOo.***°O*ooOo**o°.@..*o * .***.##o@@@@@@@.  *@  *.   .°*ooooo*oo*
-Oo °° *....°. °°°*.°o°  . °O°*°***°*o**°.**°*°°***o*ooO.*O .*° .O.°*o#.o@#@@@* °.O@*.*°o***    °*ooOOO#
-°.°°**° o**oo*oOoOo°**O. .  °**o°°*o°..o**.°°*oo***°oOoOo**#Ooo#° *o  .@@#@@@* ° o@°°o.OO°oO#°°. °.°**o
-o°**°°*O*.°oO°°OO***o. #.°*   .°** .  °*°**OooOOOo*ooooooO O@o°Ooo***o@@oo°@o.°  O#O °  °°.*°.**°o.    
-°.°**o°  .O* °o*°°°° .°*  o * O#o..*°.**°°°*oOOOoo°**°*OoooOoooOO*.@@@#@OoO@*.# O@.oO##OOOoooo*o**ooOOo
-oOo .* .*O°.OO  o*  °°°°***oO#@@@#°°°°o°°°  .   *.*°.oO**OO°oOO**°o@o ..*#o°°.°O#O .@@@@@@@@@@@@@@@@@@@
-o° *@°*o. #@@°*O° O@@@@@@@@@@@@@@@@#o.°.°°.*..*   °  ooo*oOo*o °#@@. O@@o*  o#O***o@@@@@@@@@@@@@@@@@@@@
-°O@@O.O° @@@O o° O@@@@@@@@@@@@@@@@@@@@#####o°.. *o °. *. *.°°oO@O   o@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-@@@@ oO #@@@°.#..@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#oo*°*°oOO#@O°°.°#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-@@@o°O*.@@@o°oo O@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-@@@.*o O@@@o.o* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-@@@ *°°@@@@@#.o @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-@@@O°*°@@@@@@@@OO@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-@@@@@oo@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-}
-procedure paint_dragon(x, y: integer; hide: boolean = false);
+{ Procedures called by paint_dragon depending on direction }
+procedure paint_up(x, y: integer; filler, wing, eye, leg, paw, mouth: char); forward;
+procedure paint_ri(x, y: integer; filler, wing, eye, leg, paw, mouth: char); forward;
+procedure paint_dn(x, y: integer; filler, wing, eye, leg, paw, mouth: char); forward;
+procedure paint_lf(x, y: integer; filler, wing, eye, leg, paw, mouth: char); forward;
+
+procedure paint_dragon(x, y, direction: integer; hide: boolean = false);
+const
+    Up = 119;
+    Down = 115;
+    Left = 97;
+    Right = 100;
 var
     filler: char = '0';
     wing: char = '*';
@@ -103,24 +83,27 @@ var
     paw: char = '"';
     mouth: char = '=';
 begin
-    if hide then begin
-        filler := ' ';
-        wing := ' ';
-        eye := ' ';
-        leg := ' ';
-        paw := ' ';
-        mouth := ' ';
+    if hide then begin { Change all to spaces }
+        filler := ' '; wing := ' ';
+        eye := ' '; mouth := ' ';
+        leg := ' '; paw := ' ';
     end;
-    { Body }
-    TextColor(LightCyan);
-    paint(x, y-5, filler+filler);
-    paint(x-1, y-4, filler);
-    paint(x, y-3, filler);
-    paint(x-1, y-2, filler+filler+filler);
-    paint(x-1, y-1, filler+filler+filler);
-    paint(x-2, y, filler+filler+filler+filler+filler);
-    paint(x, y+1, filler);
-    paint(x-1, y+2, filler+filler+filler);
+    case direction of
+        Up:
+            paint_up(x, y, filler, wing, eye, leg, paw, mouth);
+        Right:
+            paint_ri(x, y, filler, wing, eye, leg, paw, mouth);
+        Down:
+            paint_dn(x, y, filler, wing, eye, leg, paw, mouth);
+        Left:
+            paint_lf(x, y, filler, wing, eye, leg, paw, mouth);
+    end;
+    TextColor(LightGray);
+    GotoXY(1, ScreenHeight)
+end;
+
+procedure paint_dn(x, y: integer; filler, wing, eye, leg, paw, mouth: char);
+begin
     { Wings }
     TextColor(White);
     paint(x-4, y-1, wing+wing);
@@ -143,7 +126,133 @@ begin
     TextColor(LightCyan);
     paint(x, y+3, wing);
     paint(x, y+4, mouth);
-    TextColor(LightGray);
+    { Body }
+    paint(x, y-5, filler+filler);
+    paint(x-1, y-4, filler);
+    paint(x, y-3, filler);
+    paint(x-1, y-2, filler+filler+filler);
+    paint(x-1, y-1, filler+filler+filler);
+    paint(x-2, y, filler+filler+filler+filler+filler);
+    paint(x, y+1, filler);
+    paint(x-1, y+2, filler+filler+filler)
 end;
 
+procedure paint_up(x, y: integer; filler, wing, eye, leg, paw, mouth: char);
+begin
+    { Wings }
+    TextColor(White);
+    paint(x-4, y+1, wing+wing);
+    paint(x+3, y+1, wing+wing);
+    paint(x-3, y, wing);
+    paint(x+3, y, wing);
+    { Legs }
+    TextColor(LightGray);
+    paint(x-2, y+2, leg);
+    paint(x+2, y+2, leg);
+    paint(x-2, y+1, paw);
+    paint(x+2, y+1, paw);
+    paint(x-1, y-1, leg);
+    paint(x+1, y-1, leg);
+    paint(x-2, y-2, paw);
+    paint(x+2, y-2, paw);
+    
+    TextColor(LightRed);
+    paint(x-1, y-3, eye+' '+eye);
+    TextColor(LightCyan);
+    paint(x, y-3, wing);
+    paint(x, y-4, mouth);
+
+    paint(x, y+5, filler+filler);
+    paint(x-1, y+4, filler);
+    paint(x, y+3, filler);
+    paint(x-1, y+2, filler+filler+filler);
+    paint(x-1, y+1, filler+filler+filler);
+    paint(x-2, y, filler+filler+filler+filler+filler);
+    paint(x, y-1, filler);
+    paint(x-1, y-2, filler+filler+filler)
+end;
+
+procedure paint_ri(x, y: integer; filler, wing, eye, leg, paw, mouth: char);
+begin
+    { Wings }
+    TextColor(White);
+    paint(x-1, y+3, wing);
+    paint(x-1, y-3, wing);
+    paint(x, y+2, wing);
+    paint(x, y-2, wing);
+    paint(x+1, y+1, wing);
+    paint(x+1, y-1, wing);
+    { Legs }
+    TextColor(LightGray);
+    paint(x-2, y+1, leg);
+    paint(x-1, y+1, paw);
+    paint(x-2, y-1, leg);
+    paint(x-1, y-1, paw);
+    paint(x+2, y+1, leg);
+    paint(x+2, y-1, leg);
+    { Head }
+    TextColor(LightRed);
+    paint(x+5, y+1, eye);
+    paint(x+5, y-1, eye);
+    TextColor(LightCyan);
+    paint(x+5, y, wing);
+    paint(x+6, y, mouth);
+    { Body }
+    paint(x-6, y, filler);
+    paint(x-5, y, filler);
+    paint(x-4, y, ' ');
+    paint(x-3, y, filler);
+    paint(x-2, y, filler);
+    paint(x-1, y, filler);
+    paint(x, y, filler);
+    paint(x+1, y, filler);
+    paint(x+2, y, filler);
+    paint(x+3, y, filler);
+    paint(x+4, y, filler);
+    paint(x, y-1, filler);
+    paint(x, y+1, filler);
+    paint(x-4, y+1, filler)
+end;
+
+procedure paint_lf(x, y: integer; filler, wing, eye, leg, paw, mouth: char);
+begin
+    { Wings }
+    TextColor(White);
+    paint(x+1, y+3, wing);
+    paint(x+1, y-3, wing);
+    paint(x, y+2, wing);
+    paint(x, y-2, wing);
+    paint(x-1, y+1, wing);
+    paint(x-1, y-1, wing);
+    { Legs }
+    TextColor(LightGray);
+    paint(x+2, y+1, leg);
+    paint(x+1, y+1, paw);
+    paint(x+2, y-1, leg);
+    paint(x+1, y-1, paw);
+    paint(x-2, y+1, leg);
+    paint(x-2, y-1, leg);
+    { Head }
+    TextColor(LightRed);
+    paint(x-5, y+1, eye);
+    paint(x-5, y-1, eye);
+    TextColor(LightCyan);
+    paint(x-5, y, wing);
+    paint(x-6, y, mouth);
+    { Body }
+    paint(x+6, y, filler);
+    paint(x+5, y, filler);
+    paint(x+4, y, ' ');
+    paint(x+3, y, filler);
+    paint(x+2, y, filler);
+    paint(x+1, y, filler);
+    paint(x, y, filler);
+    paint(x-1, y, filler);
+    paint(x-2, y, filler);
+    paint(x-3, y, filler);
+    paint(x-4, y, filler);
+    paint(x, y-1, filler);
+    paint(x, y+1, filler);
+    paint(x+4, y+1, filler)
+end;
 end.
--- program.pas
+++ program.pas
@@ -1,20 +1,6 @@
 program Trash_Game; {program.pas}
 uses
-    crt, dragon;
-
-procedure GetKey(var code: integer); { Keypress handler }
-var
-    c: char;
-begin
-    c := ReadKey;
-    if c = #0 then begin { extended key }
-        c := ReadKey;
-        code := -ord(c)
-    end
-    else begin           { regular key }
-        code := ord(c)
-    end
-end;
+    crt, painter;
 
 type
     point = record
@@ -30,33 +16,54 @@ const
     Enter = 13;
     Esc = 27;
     Space = 32;
-    Up = -72;
-    Down = -80;
-    Left = -75;
-    Right = -77;
+    Up = 119;
+    Down = 115;
+    Left = 97;
+    Right = 100;
 
-procedure Play(Center: point);
+procedure GetKey(var code: integer); { Keypress handler }
 var
-    Trash: point; { Dragon's location }
-    hide: boolean = true;
+    c: char;
 begin
-    clrscr;
-    { Paint the dragon at center }
-    Trash := Center;
-    paint_dragon(Trash.x, Trash.y);
-    Delay(2000);
-    paint_dragon(Trash.x, Trash.y, hide);
+    c := ReadKey;
+    if c = #0 then begin { extended key }
+        c := ReadKey;
+        code := -ord(c)
+    end
+    else begin           { regular key }
+        code := ord(c)
+    end
 end;
 
-procedure Info(Center: point);
+procedure Play(Center: point); forward;
+procedure Info(Center: point); forward;
+procedure paint_buttons(sel_b, pl_b, in_b, ex_b: button); forward;
+procedure move_menu_cursor(key: integer; var sel_b, pl_b, in_b, ex_b: button);
+                                                                      forward;
+procedure Menu(Center: point);
 var
     key: integer;
+    play_b, info_b, exit_b, selected_b: button;
 begin
-    clrscr;
-    write('Iriska');
+    play_b.name := '   ~PLAY~   '; play_b.loc := 33;
+    info_b.name := '   ?INFO?   '; info_b.loc := 34;
+    exit_b.name := '   *EXIT*   '; exit_b.loc := 35;
+    selected_b := play_b;
+    paint_image();
+    { Let user choose a button }
     repeat
+        paint_buttons(selected_b, play_b, info_b, exit_b);
         GetKey(key);
-    until (key = Esc);
+        move_menu_cursor(key, selected_b, play_b, info_b, exit_b);
+    until (key = Enter);
+    if selected_b.loc = play_b.loc then
+        Play(Center);
+    if selected_b.loc = info_b.loc then
+        Info(Center);
+    if selected_b.loc = exit_b.loc then begin
+        write(#10#10'Goodbye!'#10);
+        halt(0)
+    end;
 end;
 
 procedure paint_buttons(sel_b, pl_b, in_b, ex_b: button);
@@ -97,32 +104,81 @@ begin
     end;
 end;
 
-procedure Menu(Center: point);
+procedure flamethrower(x, y, direction: integer); forward;
+procedure step(var key, direction, x, y: integer); forward;
+procedure Play(Center: point);
+var
+    Trash: point; { Dragon's location }
+    key: integer;
+    direction: integer = 119;
+begin
+    clrscr;
+    paint(1, 1, 'Press Esc to die');
+    { Paint the dragon at center }
+    Trash := Center;
+    paint_dragon(Trash.x, Trash.y, direction);
+    repeat
+        GetKey(key);
+        if key = Space then
+            flamethrower(Trash.x, Trash.y, direction);
+        if (key = Up) or (key = Down) or (key = Right) or (key = Left) then
+            step(key, direction, Trash.x, Trash.y);
+        GotoXY(1, ScreenHeight);
+    until (key = Esc);
+    clrscr;
+    Menu(Center)
+end;
+
+procedure flamethrower(x, y, direction: integer);
+const
+    delayDuration = 100;
+begin
+    paint(1, 2, 'Flamethrowing');
+    delay(delayDuration);
+    paint(14, 2, '.');
+    delay(delayDuration);
+    paint(15, 2, '.');
+    delay(delayDuration);
+    paint(16, 2, '.');
+    delay(delayDuration);
+    paint(1, 2, '                ');
+end;
+
+procedure step(var key, direction, x, y: integer);
+var
+    hide: boolean = true;
+begin
+    paint_dragon(x, y, direction, hide);
+    direction := key;
+    case direction of
+        Up:
+            if y > 5 then
+                y := y - 1;
+        Down:
+            if y < ScreenHeight-4 then
+                y := y + 1;
+        Right:
+            if x < ScreenWidth-7 then
+                x := x + 2;
+        Left:
+            if x > 7 then
+                x := x - 2;
+    end;
+    paint_dragon(x, y, direction);
+    GotoXY(1, 2); write('('); write(x); write(', '); write(y); write(')');
+end;
+
+procedure Info(Center: point);
 var
     key: integer;
-    play_b, info_b, exit_b, selected_b: button;
 begin
-    play_b.name := '   ~PLAY~   '; play_b.loc := 33;
-    info_b.name := '   ?INFO?   '; info_b.loc := 34;
-    exit_b.name := '   *EXIT*   '; exit_b.loc := 35;
-    selected_b := play_b;
-    paint_image();
-    { Let user choose a button }
+    clrscr;
+    write('Iriska');
     repeat
-        paint_buttons(selected_b, play_b, info_b, exit_b);
         GetKey(key);
-        move_menu_cursor(key, selected_b, play_b, info_b, exit_b);
-    until (key = Enter);
-    { instruction 'case of' doesn't work in this situation }
-    { because of 'constant expression required' }
-    if selected_b.loc = play_b.loc then
-        Play(Center);
-    if selected_b.loc = info_b.loc then
-        Info(Center);
-    if selected_b.loc = exit_b.loc then begin
-        write(#10#10'Goodbye!'#10);
-        halt(0)
-    end;
+    until ((key = Esc) or (key = Enter));
+    clrscr;
+    Menu(Center);
 end;
 
 var
