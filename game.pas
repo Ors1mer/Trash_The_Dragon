a704315195c056064b9ee5ed310846b9cd65abf3
Ors1mer <jakonhovland96@gmail.com>
2022-06-23 17:10:52 +0300

    Names and notifications added

--- game.pas
+++ game.pas
@@ -36,12 +36,12 @@ var
     key: integer;
     play_b, info_b, exit_b, selected_b: button;
 begin
-    play_b.name := '   ~PLAY~   '; play_b.loc := 33;
-    info_b.name := '   ?INFO?   '; info_b.loc := 34;
-    exit_b.name := '   *EXIT*   '; exit_b.loc := 35;
+    play_b.name := '    ~PLAY~   '; play_b.loc := 33;
+    info_b.name := '    ?INFO?   '; info_b.loc := 34;
+    exit_b.name := '    *EXIT*   '; exit_b.loc := 35;
     selected_b := play_b;
     paint_image();
-    { Let user choose a button }
+    { Let user choose button }
     repeat
         paint_buttons(selected_b, play_b, info_b, exit_b);
         GetKey(key);
@@ -58,14 +58,16 @@ begin
 end;
 
 procedure paint_buttons(sel_b, pl_b, in_b, ex_b: button);
+var
+    x_loc: integer = (102 - 8) div 2;
 begin
-    paint((103 - 8) div 2, pl_b.loc, pl_b.name);
-    paint((103 - 8) div 2, in_b.loc, in_b.name);
-    paint((103 - 8) div 2, ex_b.loc, ex_b.name);
-    GotoXY((103 - 8) div 2, sel_b.loc);
+    paint(x_loc, pl_b.loc, pl_b.name);
+    paint(x_loc, in_b.loc, in_b.name);
+    paint(x_loc, ex_b.loc, ex_b.name);
+    GotoXY(x_loc, sel_b.loc);
     TextBackground(ButtonBg); TextColor(ButtonCol);
     write(sel_b.name);
-    GotoXY((103 - 8) div 2, WhereY);
+    GotoXY(x_loc, WhereY);
     TextBackground(DefaultBg); TextColor(DefaultCol);
 end;
 
@@ -99,9 +101,17 @@ end;
 
 const
     BiAmount = 9;
+    BiNames: array[1..9] of string = (
+        'Keons', 'Sacarver', 'Listo',
+        'Lisden', 'Reisdro', 'Vetomo',
+        'Andre', 'Nico', 'Nills'
+    );
+    aliveBi = 1;
+    resurrectBi = -64;
 type
     Bishop = record
-        state: integer; { <1 - dead, 1 - alive }
+        name: string;
+        state: integer; { <1 - dead, 1 - aliveBi }
         dir: integer;
         x: integer;
         y: integer;
@@ -132,7 +142,7 @@ begin
         if key = Space then
             flamethrower(Trash.x, Trash.y, direction, Niners)
         else if is_move(key) then
-            step(key, direction, Trash.x, Trash.y);
+            step(key, direction, Trash.x, Trash.y)
     until (key = Esc);
     clrscr;
     Menu(Center)
@@ -140,43 +150,63 @@ end;
 
 function is_move(key: integer): boolean;
 begin
-    is_move := (key=Up) or (key=Down) or (key=Left) or (key=Right);
+    is_move := (key=Up) or (key=Down) or (key=Left) or (key=Right)
 end;
 
 procedure get_default_bishops(var Niners: Bishops); forward;
+procedure resurrection(var Bi: Bishop); forward;
 procedure spawn_bishops(var Niners: Bishops);
-var
-    n: integer;
+const
     dir: array[1..2] of integer = (Left, Right);
     hide: boolean = true;
-    alive: integer = 1;
+var
+    n: integer;
 begin
-    if Niners[1].x = -1 then {bishops weren't printed yet}
+    if Niners[1].x = -1 then { bishops weren't printed yet }
         get_default_bishops(Niners);
 
     for n := 1 to BiAmount do begin
-        if Niners[n].state = alive then begin
+        resurrection(Niners[n]);
+
+        if Niners[n].state = aliveBi then begin
             paint_bishop(Niners[n].x, Niners[n].y, Niners[n].dir, hide);
             Niners[n].dir := dir[random(3)];
-            paint_bishop(Niners[n].x, Niners[n].y, Niners[n].dir);
-        end;
+            paint_bishop(Niners[n].x, Niners[n].y, Niners[n].dir)
+        end else begin
+            Niners[n].state := Niners[n].state - 1
+        end
     end;
     GotoXY(1, ScreenHeight)
+end;
+
+procedure resurrection(var Bi: Bishop);
+const
+    msg = ' resurrected!';
+begin
+    if Bi.state = resurrectBi then begin
+        Bi.State := aliveBi;
+        GotoXY(3, ScreenHeight);
+        clreol;
+        TextColor(BishopEyeCol);
+        write(Bi.name+msg);
+        TextColor(DefaultCol);
+    end
 end;
 
 procedure get_default_bishops(var Niners: Bishops);
 var
     n, x, y, kx, ky: integer;
-    alive: integer = 1;
+    aliveBi: integer = 1;
 begin
     x := ScreenWidth div 4;
     y := ScreenHeight div 4;
-    n := 1; {bishop's number}
+    n := 1; { bishop's number }
     for ky := 1 to 3 do begin
         for kx := 1 to 3 do begin
+            Niners[n].name := BiNames[n];
+            Niners[n].state := aliveBi;
             Niners[n].x := kx*x;
             Niners[n].y := ky*y;
-            Niners[n].state := alive;
             n := n + 1;
         end;
     end;
@@ -184,41 +214,46 @@ end;
 
 procedure aim_bi_origin(a, a1, b, b1, w: integer; var Bi: Bishop); forward;
 procedure hit_bishop(x, y, d, wave: integer; var Nrs: Bishops);
+const
+    DrFrontSizeY = 4;
+    DrFrontSizeX = 6;
 var
-    DrFrontSizeY: integer = 4;
-    DrFrontSizeX: integer = 6;
     n: integer;
 begin
     case d of
         Up:    y := y - wave - DrFrontSizeY;
         Down:  y := y + wave + DrFrontSizeY;
         Left:  x := x - wave - DrFrontSizeX;
-        Right: x := x + wave + DrFrontSizeX;
+        Right: x := x + wave + DrFrontSizeX
     end;
     for n := 1 to BiAmount do begin
         case d of
             Up, Down:
                 aim_bi_origin(x, Nrs[n].x, y, Nrs[n].y, wave, Nrs[n]);
             Left, Right:
-                aim_bi_origin(y, Nrs[n].y, x, Nrs[n].x, wave, Nrs[n]);
-        end;
-    end;
+                aim_bi_origin(y, Nrs[n].y, x, Nrs[n].x, wave, Nrs[n])
+        end
+    end
 end;
 
 procedure aim_bi_origin(a, a1, b, b1, w: integer; var Bi: Bishop);
+const
+    hide: boolean = true;
+    msg = ' scorched!';
 var
     i: integer;
-    hide: boolean = true;
 begin
     if b = b1 then begin
         for i := a-w to a+w do begin
             if i = a1 then begin
                 Bi.state := Bi.state - 1;
+                TextColor(KnifeCol1);
+                paint(3, ScreenHeight, Bi.name+msg);
                 paint_bishop(Bi.x, Bi.y, Bi.dir, hide);
-                break;
-            end;
-        end;
-    end;
+                break
+            end
+        end
+    end
 end;
 
 procedure flamethrower(x, y, d: integer; var Niners: Bishops);
@@ -226,6 +261,7 @@ var
     SentenceLen: integer = 13;
     w: integer;
 begin
+    GotoXY(3, ScreenHeight); clreol;
     paint(1, 2, 'Flamethrowing');
     for w := 1 to 3 do begin
         paint(SentenceLen+w, 2, '.');
@@ -268,10 +304,9 @@ var
     dragon_txt, bishop_txt, msg: string;
 begin
     dragon_txt := 'This is Trash, a dragon.'#10 +
-    'He knows how to fire-breathe.'#10;
+    'He''s good at fire-breathing.'#10;
     bishop_txt := 'This is one of the nine bishops.'#10 +
-    'These are their names:'#10'Andre, Nico, Keons,'#10'Savarver, ' +
-    'Lisden, Reisdro,'#10'Nills, Vetomo, Listo.'#10 +
+    'Each of them has a name.'#10 +
     'They wear a red cloak with a hood.'#10'And carry knifes.'#10;
     msg := 'You are flamethrower, they''re - switchblades.'#10;
 
@@ -280,7 +315,8 @@ begin
     paint_dragon(15, WhereY+6, Down);
     paint(0, 15, bishop_txt);
     paint_bishop(16, WhereY+3, Left);
-    paint(1, 29, msg);
+    paint(1, 26, msg);
+    GotoXY(1, ScreenHeight);
     repeat
         GetKey(key);
     until ((key = Esc) or (key = Enter));
